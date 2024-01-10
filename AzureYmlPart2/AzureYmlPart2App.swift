d8045985b4020c711d39e83c6cffdcbc696ab6ea
erdi ergene <erdieergene@gmail.com>
2024-01-10 23:39:46 +0300

    Initial Commit

---
+++ AzureYmlPart2/AzureYmlPart2App.swift
@@ -0,0 +1,17 @@
+//
+//  AzureYmlPart2App.swift
+//  AzureYmlPart2
+//
+//  Created by Erdi Ergene on 10.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct AzureYmlPart2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
